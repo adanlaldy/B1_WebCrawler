1f94f9e5b8a40696b516a041ac0fbb9246aad987
Florent Detres <florent.detres@protonmail.com>
2023-05-23 14:33:29 +0200

    test crawler audemar

--- server.js
+++ server.js
@@ -1,34 +1,46 @@
-// EXPRESS crée notre serveur web
 const express = require('express');
+const puppeteer = require('puppeteer');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-  });
-  const port = 3000; 
-  app.listen(port, () => {
-    console.log(`Serveur en écoute sur le port ${port}`);
-  });
-    
-// PUPPETEER permet de crawler les pages web qu'on lui fournit
-const puppeteer = require('puppeteer');
 app.get('/crawl', async (req, res) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    // Ici, vous pouvez écrire le code pour naviguer vers les pages souhaitées et extraire les données
-    // Exemple : Naviguer vers une page et extraire le titre
-    await page.goto('https://www.example.com');
-    const pageTitle = await page.title();
-    console.log('Titre de la page :', pageTitle);
-
-
+    
+    // Naviguer vers le site Audemars Piguet
+    await page.goto('https://www.audemarspiguet.com/com/fr/home.html');
+    
+    // Récupérer les informations souhaitées
+    const watches = await page.evaluate(() => {
+      // Sélectionnez les éléments contenant les informations des montres
+      const watchElements = Array.from(document.querySelectorAll('.watch-element'));
+      
+      // Extrayez les informations nécessaires pour chaque montre
+      const watchData = watchElements.map((element) => {
+        const name = element.querySelector('.watch-name').innerText;
+        const price = element.querySelector('.watch-price').innerText;
+        const description = element.querySelector('.watch-description').innerText;
+        
+        return {
+          name,
+          price,
+          description
+        };
+      });
+      
+      return watchData;
+    });
+    
     await browser.close();
-
-    res.send('Crawling terminé !');
+    
+    res.json(watches);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Erreur lors du crawling');
+    console.error('Une erreur s\'est produite :', error);
+    res.status(500).send('Une erreur s\'est produite lors du crawling du site.');
   }
+});
+
+app.listen(3000, () => {
+  console.log('Serveur en écoute sur le port 3000');
 });
