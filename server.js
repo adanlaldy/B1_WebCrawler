cb2f0026f2bae39ab6aa8125d1d7cd09d90599c9
Florent Detres <florent.detres@protonmail.com>
2023-05-23 14:08:04 +0200

    adding puppeteer

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ app.get('/', (req, res) => {
     console.log(`Serveur en écoute sur le port ${port}`);
   });
     
+
+const puppeteer = require('puppeteer');
+app.get('/crawl', async (req, res) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Ici, vous pouvez écrire le code pour naviguer vers les pages souhaitées et extraire les données
+    // Exemple : Naviguer vers une page et extraire le titre
+    await page.goto('https://www.example.com');
+    const pageTitle = await page.title();
+    console.log('Titre de la page :', pageTitle);
+
+
+    await browser.close();
+
+    res.send('Crawling terminé !');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erreur lors du crawling');
+  }
+});
