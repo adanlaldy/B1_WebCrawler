107242ad45296271476d9d7368fce3162e333d54
Florent Detres <florent.detres@protonmail.com>
2023-05-24 16:42:00 +0200

    modif

--- server.js
+++ server.js
@@ -3,24 +3,29 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 
+
+
 app.get('/', async (req, res) => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
+
     // Naviguer vers le site Audemars Piguet
     await page.goto('https://www.omegawatches.com/fr-fr/watches/constellation/globemaster/catalog');
-    
-    const element = await page.waitForSelector('div > #product-list-cont');
-    const elementContent = await page.evaluate(el => el.innerHTML, element);
-    
+
+    const elements = await page.$$('div > #product-list-cont');
+
+    const elementContent = await page.evaluate((...elements) => {
+      return elements.map(el => `<div>${el.innerHTML}</div>`).join('\n');
+    }, ...elements);
+
     res.send(elementContent);
 
     await browser.close();
 
   } catch (error) {
     console.error('Une erreur s\'est produite :', error);
-    res.status(404).sendFile('..\assets\html\404.html');
+    res.status(404).sendFile('../assets/html/404.html');
   }
 });
 
