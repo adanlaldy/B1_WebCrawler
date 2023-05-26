8631c86e09dcd4fe24057e9577092c7490788ddb
Anael Lasserre <anael.lasserre@ynov.com>
2023-05-26 17:52:29 +0200

    BIG UPDATE !!! SCRAPPER CLOSE TO END !

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static('assets/css'));
 
+app.get('/assets/css/main.css', (req, res) => {
+    res.setHeader('Content-Type', 'text/css');
+    res.sendFile(path.join(__dirname,'/assets/css/main.css'));
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'assets', 'template'));
 
@@ -14,20 +19,46 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'assets', 'html', 'main.html'));
+    res.sendFile(path.join(__dirname, 'assets', 'html', 'index.html'));
   });
 
-app.get('/crawl', async (req, res) => {
-  try {
-    // Lancer le navigateur
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
+app.get('/both', async (req, res) => {
+    let watches = await scrapOmega();
+    watches.concat(await scrapWatchFinder());
 
-    // Créer une page
-    const page = await browser.newPage();
+    watches.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
 
-    let watches = [];
+    res.render('main', { data: watches });
+})
+
+app.get('/omega', async (req, res) => {
+    let watches = await scrapOmega();
+    if (watches != null) {
+        watches.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    }
+    res.render('omega', { data: watches });
+})
+
+app.get('/watchfinder', async (req, res) => {
+    let watches = await scrapWatchFinder();
+    if (watches != null) {
+        watches.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    }
+    res.render('watchfinder', { data: watches });
+})
+
+app.get('**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'assets', 'html', '404.html'));
+});
+
+async function scrapOmega() {
+    try {
+        // Lancer le navigateur
+        const browser = await puppeteer.launch({
+            headless: "new",
+        });
+        // Créer une page
+        const page = await browser.newPage();
         // Liste des URLs des pages à parcourir
         const urls = [
             'https://www.omegawatches.com/fr-fr/watches/constellation/globemaster/catalog',
@@ -47,67 +78,11 @@ app.get('/crawl', async (req, res) => {
             'https://www.omegawatches.com/fr-fr/watches/de-ville/prestige/catalog',
             'https://www.omegawatches.com/fr-fr/watches/de-ville/tourbillon/catalog',
         ];
-
         // Tableau pour stocker le contenu des éléments
         let watches = [];
-
-    for (let index = 0; index <= 13; index++) {
-      // Accéder à votre site
-      await Promise.all([
-        page.goto(`https://www.watchfinder.fr/Rolex/watches/all?pageno=${index}`),
-        page.waitForNavigation({ waitUntil: 'networkidle0' }),
-      ]);
-  
-      await page.waitForSelector('picture img');
-  
-      /*
-      div globale : 'div[data-testid="searchResultContainer"]'
-      div des montres : 'div[data-testid="watchItem"]'
-      info : 'a[data-testid="watchLink"]'
-      model : 'a[data-testid="watchLink"] div[data-testid="watchSeries"]'
-      prix : 'a[data-testid="watchLink"] div[data-testid="watchPrice"]'
-      image : 'a[data-testid="watchLink"] img'
-       */
-  
-      // Recupère la liste
-      const watchesList = await page.$$('div.row div[data-testid="searchResultContainer"] div[data-testid="watchItem"]');
-  
-      
-      // Recupère les éléments
-      for (let i = 0; i < watchesList.length; i++) {
-        var img = "";
-        getImgLink(watchesList[i])
-        .then(resp => img = resp);
-  
-        watches.push({
-          "model": await watchesList[i].$eval('div[data-testid="watchSeries"]', el => el.textContent),
-          "price": await watchesList[i].$eval('div[data-testid="watchPrice"]', el => el.textContent),
-          "img": img,
-        });
-
-        console.log('WatchesList ', i, ": ", watches[i]);
-      }
-    }
-
-    browser.close();
-
-    console.log('Total WatchesList length: ', watches.length);
-
-    // Envoyer le contenu de l'élément dans la réponse
-    res.render('main', { data: watches });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(`
-    <div>
-      <h1>Erreur lors du crawling</h1>
-    </div>
-    `);
-  }
         for (const url of urls) {
             // Accéder à la page spécifique
             await page.goto(url);
-
             const elements = await page.$$('ol#product-list-grid .product-item');
             for (let i = 0; i < elements.length; i++) {
                 const model = await elements[i].$eval('.ow-prod__desc-top .name', el => el.textContent);
@@ -122,37 +97,98 @@ app.get('/crawl', async (req, res) => {
                 });
             }
         }
-        // Tri du tableau par prix croissant
-        watches.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-
-        res.render('main', {data: watches});
+        return watches;
     } catch (error) {
         console.error(error);
-        res.status(500).send(`
-    <div>
-      <h1>Erreur lors du crawling</h1>
-    </div>
-    `);
+        return null;
     }
-});
+}
 
-async function getImgLink(currentWatch) {
-  const img = await currentWatch.$eval('picture img', el => el.getAttribute('srcset'));
-  const goodLink = getFirstLink(img);
+async function scrapWatchFinder() {
+    try {
+        // Lancer le navigateur
+        const browser = await puppeteer.launch({
+            headless: "new",
+        });
 
-  if (goodLink == null) {
-    return currentWatch.$eval('picture img', el => el.getAttribute('src'));
-  }
+        // Créer une page
+        const page = await browser.newPage();
 
-  return goodLink;
-};
+        let watches = [];
 
+        for (let index = 1; index <= 13; index++) {
+            // Accéder à votre site
+            await Promise.all([
+                page.goto(`https://www.watchfinder.fr/Rolex/watches/all?pageno=${index}`),
+                page.waitForNavigation({waitUntil: 'networkidle0'}),
+            ]);
+
+            await page.waitForSelector('picture img');
+
+            await autoScroll(page);
+
+            /*
+            div globale : 'div[data-testid="searchResultContainer"]'
+            div des montres : 'div[data-testid="watchItem"]'
+            info : 'a[data-testid="watchLink"]'
+            model : 'a[data-testid="watchLink"] div[data-testid="watchSeries"]'
+            prix : 'a[data-testid="watchLink"] div[data-testid="watchPrice"]'
+            image : 'a[data-testid="watchLink"] img'
+             */
+
+            // Recupère la liste
+            const watchesList = await page.$$('div.row div[data-testid="searchResultContainer"] div[data-testid="watchItem"]');
+
+            // Recupère les éléments
+            for (let i = 0; i < watchesList.length; i++) {
+                let img = await watchesList[i].$eval('picture img', el => el.getAttribute('srcset'));
+                let price;
+                try {
+                    price = await watchesList[i].$eval('div[data-testid="watchPrice"]', el => el.textContent);
+                } catch (_) {
+                    price = await watchesList[i].$eval('span[data-testid="watchPrice"]', el => el.textContent);
+                }
+
+                watches.push({
+                    "model": await watchesList[i].$eval('div[data-testid="watchSeries"]', el => el.textContent),
+                    "price": price,
+                    "img": getFirstLink(img),
+                });
+            }
+        }
+
+        await browser.close();
+
+        return watches;
+
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
 function getFirstLink(dataSrcset) {
-  if (dataSrcset != null) {
-    const links = dataSrcset.split(", ");
-    const firstLink = links[0].split(" ")[0];
-    return firstLink;
-  } else {
-    return null;
-  }
+    if (dataSrcset != null) {
+        const links = dataSrcset.split(", ");
+        return links[0].split(" ")[0];
+    } else {
+        return null;
+    }
+}
+ async function autoScroll(page) {
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            let totalHeight = 0;
+            const distance = 200;
+            const scrollInterval = setInterval(() => {
+                const scrollHeight = document.documentElement.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight) {
+                    clearInterval(scrollInterval);
+                    resolve();
+                }
+                }, 2);
+        });
+    });
 }
